aa707d87609d14d9525044d0746465a4a59bad0c
ClausKlein <claus.klein@arcormail.de>
2020-12-05 22:25:58 +0100

    fix typo

--- cert-MEM55.cpp
+++ cert-MEM55.cpp
@@ -6,14 +6,14 @@
 #include <new>
 
 // warning: do not manage memory manually; use RAII [cppcoreguidelines-no-malloc]
-void* operator new(std::size_t size) { return std::malloc(size); }
+void* operator new(std::size_t size) /*** noexcept ***/ { return std::malloc(size); }
 
 void operator delete(void* ptr) noexcept { std::free(ptr); }
 void operator delete(void* ptr, std::size_t) noexcept; // Defined elsewhere
 
 int main()
 {
-    int* p = new int;   // diagnositc requiered
+    int* p = new int;   // diagnostic required
     // ...
     delete p;
     // warning: Potential leak of memory pointed to by 'p' [clang-analyzer-unix.Malloc]
