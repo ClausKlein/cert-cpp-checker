f2b0719a38aa438d0ee89aaea7ce880eb0a70adc
ClausKlein <claus.klein@arcormail.de>
2020-12-15 18:51:18 +0100

    forget linux, use OSX

--- cert-CTR55.cpp
+++ cert-CTR55.cpp
@@ -12,23 +12,26 @@ void bad(const std::vector<int>& c)
 {
     constexpr size_t maxSize = 20;
     for (auto i = c.begin(), e = i + maxSize; i != e; ++i) {
-        std::cout << *i << std::endl; // diagnostic required
+        std::cout << *i << ", "; // diagnostic required
     }
+    std::cout << std::endl;
 }
 
 void good(const std::vector<int>& c)
 {
     constexpr std::vector<int>::size_type maxSize = 20;
     for (auto i = c.begin(), e = i + std::min(maxSize, c.size()); i != e; ++i) {
-        std::cout << *i << std::endl;
+        std::cout << *i << ", ";
     }
+    std::cout << std::endl;
 }
 
 void perfect(const std::vector<int>& c)
 {
     for (const auto& i : c) {
-        std::cout << i << std::endl;
+        std::cout << i << ", ";
     }
+    std::cout << std::endl;
 }
 
 } // namespace
