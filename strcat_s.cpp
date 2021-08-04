5e78b0f9e402c56fc8f67d40f45ead7047e3eb21
ClausKlein <claus.klein@arcormail.de>
2021-08-04 16:26:37 +0200

    fix strlcat according to the BSD manual

--- strcat_s.cpp
+++ strcat_s.cpp
@@ -1,58 +1,68 @@
 #define __STDC_WANT_LIB_EXT1__ 1
+#include <cassert>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <cassert>
 
 namespace {
 
 #if !(defined(__APPLE__) || defined(__BSD__))
-// If the return value is >= dstsize, the output string has been
-// truncated.  It is the caller's responsibility to handle this
+// Like snprintf(3), the strlcpy() and strlcat() functions return the
+// total length of the string they tried to create.
 size_t strlcpy(char* dst, const char* src, size_t capacity)
 {
+    assert(dst != nullptr);
     int len = snprintf(dst, capacity, "%s", src);
     printf("dst = \"%s\", len = %d, capacity = %zu\n", dst, len, capacity);
 
-    if(len >= 0 && (size_t)len >= capacity) {
+    if (len >= 0 && (size_t)len >= capacity) {
         puts("Warning: truncated str at strlcpy()!");
     }
 
     return len;
 }
 
+// If the return value is >= dstsize, the output string has been
+// truncated.  It is the caller's responsibility to handle this
 size_t strlcat(char* dst, const char* src, size_t capacity)
 {
+    assert(dst != nullptr);
     size_t actual_used = strlen(dst);
+    assert(actual_used < capacity);
+
     if (capacity > (actual_used + 1)) {
         return strlcpy(dst + actual_used, src, capacity - actual_used);
     }
 
-    return actual_used;
+    return actual_used + strlen(src); // NOTE: total_len
 }
 #endif
-}
+
+} // namespace
 
 int main(void)
 {
     char dst[15 + 2 * 6 + 1] = "Hello ";
     char src[6 + 1] = "World!";
-    constexpr size_t capacity = sizeof(dst);
+    constexpr size_t capacity = sizeof(dst); // NOTE: 28
 
-#if 1 // defined(__APPLE__) || defined(__BSD__)
-    size_t actual_len = strlcat(dst, src, capacity);
-    actual_len = strlcat(dst, " .............#", capacity);
-    if (actual_len >= capacity) {
+    size_t total_len = strlcat(dst, src, capacity);
+    total_len = strlcat(dst, " .............#", capacity);
+    printf("dst = \"%s\", len = %zu, capacity = %zu\n", dst, total_len, capacity);
+    if (total_len >= capacity) {
         puts("Warning: truncated string!");
     }
-    //XXX else
+    // XXX else
     {
-        actual_len = strlcat(dst, " Goodbye World!", capacity);
-        printf("dst = \"%s\", len = %zu, capacity = %zu\n", dst, actual_len, capacity);
-        assert(actual_len < capacity);
+        total_len = strlcat(dst, " Goodbye World!", capacity);
+        printf("dst = \"%s\", len = %zu, capacity = %zu\n", dst, total_len, capacity);
+        assert(total_len == 42);
     }
-    //XXX puts(dst);
-#endif
+    // XXX puts(dst);
+
+    total_len = strlcpy(dst, "Hello World! .............#1234567890", capacity);
+    printf("dst = \"%s\", len = %zu, capacity = %zu\n", dst, total_len, capacity);
+    assert(total_len == 37);
 
 #if defined(__STDC_LIB_EXT1__) || defined(_WIN32)
     set_constraint_handler_s(ignore_handler_s);
